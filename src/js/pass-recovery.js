e7f200c7fd8ce9b7bd1c79f12cb3ba3317963fc1
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-09-14 17:12:50 +0300

    added password recovery

---
+++ src/js/pass-recovery.js
@@ -0,0 +1,42 @@
+//import { signInWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
+import { Validator } from "./utilits/classes.js";
+//import { auth, showLoginError } from "./modules/firebase.js";
+
+const form = document.forms.sign_in;
+const elForm = form.elements;
+const regValidator = new Validator(form);
+const submitBtn = form.querySelector('.recover__submit');
+
+regValidator.blurValidation();
+
+for (let i = 0; i < form.elements.length; i++) {
+   const element = form.elements[i];
+   element.addEventListener('input', validationForBtn);
+}
+function validationForBtn() {
+   if (regValidator.backgroundValdation()) {
+      submitBtn.classList.remove('no-active-button');
+   } else {
+      submitBtn.classList.add('no-active-button');
+
+   }
+}
+form.addEventListener('submit', submitFormHandler);
+function submitFormHandler(event) {
+   event.preventDefault();
+
+   if (regValidator.validation()) {
+      try {
+         submitBtn.classList.add('no-active-button');
+         submitBtn.disabled = true;
+         submitBtn.style.cursor = 'not-allowed';
+
+         window.location.replace("subscriptions.html")
+      } catch (error) {
+         submitBtn.classList.remove('no-active-button')
+         submitBtn.disabled = false;
+         submitBtn.style.cursor = 'pointer';
+         console.log(error);
+      }
+   }
+}
